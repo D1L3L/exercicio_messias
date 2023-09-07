5e5e1afbc0910cc59f8ae3f1baa1715b8a0e1d53
Dani Oliveira <86899014+D1L3L@users.noreply.github.com>
2023-09-07 02:43:55 -0300

    exercício 8

---
+++ exercício 8.py
@@ -0,0 +1,6 @@
+#Faça um programa que calcule a média de uma lista de números usando um loop for.
+lista = [7 ,8 ,9, 6, 9, 7, 7, 5, 8, 4]
+divisor = len(lista)
+soma = sum(lista)
+resultado = soma/divisor
+print(resultado)
---
+++ exercício 9.py
@@ -0,0 +1,2 @@
+#Escreva um programa que conte quantas vezes a letra "a" aparece em uma string usando um loop for.
+
