c5a3ed1f5dc64ce737cb17b3817aed855d2ad10c
Dani Oliveira <86899014+D1L3L@users.noreply.github.com>
2023-09-07 21:10:31 -0300

    exercício 9

---
+++ exercício 10.py
@@ -0,0 +1,1 @@
+#Crie um programa que imprima todos os números primos de 1 a 50 usando um loop for.
--- exercício 9.py
+++ exercício 9.py
@@ -1,2 +1,13 @@
 #Escreva um programa que conte quantas vezes a letra "a" aparece em uma string usando um loop for.
-
+palavras = str(input("Digite uma palavra: "))
+letras = (len(palavras))
+contador = 0
+for x in range(letras):
+    if (palavras[x]) == "a" or (palavras[x]) == "á" or (palavras[x]) == "à" or (palavras[x]) == "â":
+        contador += 1
+if contador == 1:
+    print (f"A palavra digitada foi {palavras} ela possui {letras} letras, e contém apenas {contador} letra a.")
+elif contador > 1:
+    print (f"A palavra digitada foi {palavras} ela possui {letras} letras, e contém {contador} letras a.")
+else:
+    print (f"A palavra digitada foi {palavras} ela possui {letras} letras, mas não contém letras a.")
